04fe38a4d427795539514581801586001a721c8f
assa9630 <assa9630@gmail.com>
2025-05-25 12:13:45 +0800

    Delete strategy_runner.py

--- strategy_runner.py
+++
@@ -1,7 +0,0 @@
-from trade_manager import run_trade_logic
-from apscheduler.schedulers.background import BackgroundScheduler
-
-def run_strategy():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(run_trade_logic, "interval", minutes=15)
-    scheduler.start()
